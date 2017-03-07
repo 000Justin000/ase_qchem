c2a265c07b44ba4a0000169ef499c9c36cd2aab9
Junteng Jia <jj585@cornell.edu>
2017-03-06 21:43:28 -0500

    add different calculator

--- ase/test_ase.py
+++ ase/test_ase.py
@@ -1,13 +1,77 @@
 from ase import Atoms
 from ase.visualize import view
 from ase.optimize import BFGS
+from ase.calculators.emt import EMT
+from ase.calculators.lammpsrun import LAMMPS
 from ase.calculators.nwchem import NWChem
-from ase.constraints import FixInternals
-from ase.io import write
+from ase.constraints import FixInternals, Hookean
+from ase.io import read, write
 
-h2o = Atoms('HOH', positions=[[0.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
-h2o.calc = NWChem(xc='B3LYP')
-c = FixInternals(bonds=[[1.0, [0,1]]])
-h2o.set_constraint(c)
-opt = BFGS(h2o)
-opt.run(fmax=0.02)
+#-----------------------------------------
+# set atomic radius
+#-----------------------------------------
+radius = {'H':0.25, 'C':0.70, 'O':0.60}
+#-----------------------------------------
+
+#-----------------------------------------
+# read the molecule
+#-----------------------------------------
+mol = read("1001.xyz")
+#-----------------------------------------
+
+#-----------------------------------------
+# read the molecule, center in the cell
+#-----------------------------------------
+mol.cell = [20.0, 20.0, 20.0]
+mol.center()
+#-----------------------------------------
+
+#-----------------------------------------
+# set the constraint
+#-----------------------------------------
+natom = mol.get_number_of_atoms()
+#-----------------------------------------
+mol_atoms = mol.get_chemical_symbols()
+#-----------------------------------------
+fix_bonds = []
+#-----------------------------------------
+for i in range(0,natom):
+    for j in range(i+1,natom):
+        ratio = mol.get_distance(i,j) / (radius[mol_atoms[i]] + radius[mol_atoms[j]])
+        if (ratio < 1.25):
+            fix_bonds.append( [mol.get_distance(i,j), [i,j]] )
+#-----------------------------------------
+constraints = FixInternals(bonds = fix_bonds)
+#-----------------------------------------
+mol.set_constraint(constraints)
+#-----------------------------------------
+
+#-----------------------------------------
+# classical MD with EMT
+#-----------------------------------------
+mol.calc = EMT()
+opt = BFGS(mol, trajectory="1001.traj")
+opt.run(fmax=0.1)
+#-----------------------------------------
+
+#-----------------------------------------
+# classical MD with LAMMPS
+#-----------------------------------------
+# mol.calc = LAMMPS(tmp_dir="./lammps_tmp", )
+# opt = BFGS(mol, trajectory="1001.traj")
+# opt.run(fmax=0.1)
+#-----------------------------------------
+
+#-----------------------------------------
+# quantum NWChem
+#-----------------------------------------
+# mol.calc = NWChem(xc='B3LYP', basis='STO-3G')
+# opt = BFGS(mol)
+# opt.run(fmax=0.1)
+#-----------------------------------------
+
+#-----------------------------------------
+# show final molecule
+#-----------------------------------------
+# view(mol, viewer='Avogadro')
+#-----------------------------------------
