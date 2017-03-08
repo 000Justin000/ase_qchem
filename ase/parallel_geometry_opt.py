1098e9aaae933f73a4fb85c86545707ba8a10ac4
Junteng Jia <jj585@cornell.edu>
2017-03-07 22:53:31 -0500

    added parallel

--- ase/parallel_geometry_opt.py
+++ ase/parallel_geometry_opt.py
@@ -1,6 +1,8 @@
 #--------------------------------------------------
-import os, math
+import os, math, numpy
 import openbabel, pybel, ase
+#--------------------------------------------------
+from mpi4py import MPI
 #--------------------------------------------------
 from ase import Atoms
 from ase.visualize import view
@@ -14,10 +16,10 @@ from ase.constraints import FixInternals, Hookean
 #--------------------------------------------------
 
 #--------------------------------------------------
-def pyb2ase(pybmol):
-    pybmol.write("pdb", "tmp.pdb", overwrite=True)
-    asemol = ase.io.read("tmp.pdb")
-    os.remove("tmp.pdb")
+def pyb2ase(pybmol, pid):
+    pybmol.write("pdb", "tmp"+"{:04d}".format(pid)+".pdb", overwrite=True)
+    asemol = ase.io.read("tmp"+"{:04d}".format(pid)+".pdb")
+    os.remove("tmp"+"{:04d}".format(pid)+".pdb")
     #--------------
     return asemol
 
@@ -35,10 +37,10 @@ def geomopt(pybmol, forcefield):
     return pybmol
 
 #--------------------------------------------------
-def pybview(pybmol):
-    pybmol.write("pdb", "tmp.pdb", overwrite=True)
-    os.system("avogadro tmp.pdb")
-    os.remove("tmp.pdb")
+def pybview(pybmol, pid):
+    pybmol.write("pdb", "tmp"+"{:04d}".format(pid)+".pdb", overwrite=True)
+    os.system("avogadro tmp"+"{:04d}".format(pid)+".pdb")
+    os.remove("tmp"+"{:04d}".format(pid)+".pdb")
 
 #--------------------------------------------------
 def getRMSD(pybmol1, pybmol2):
@@ -53,10 +55,26 @@ def getCoords(pybmol):
         coords.append( list(atom.coords) )
     return coords
 
+#--------------------------------------------------
+def getPybmol(pybmol, coords):
+    molr = pybmol.clone
+    for atom, coord in zip(molr, coords):
+        atom.OBAtom.SetVector(coord[0], coord[1], coord[2])
+    return molr
+
 
 #################################################
 #           main function start here            #
 #################################################
+
+#------------------------------------------------
+#           initialize mpi parameters           #
+#------------------------------------------------
+nproc = MPI.COMM_WORLD.Get_size()
+iproc = MPI.COMM_WORLD.Get_rank()
+print "nproc = ", nproc, "iproc = ", iproc
+#------------------------------------------------
+
 #------------------------------------------------
 #         read the molecule with pybel          #
 #------------------------------------------------
@@ -71,49 +89,78 @@ pybmol = geomopt(pybmol, 'mmff94s')
 
 
 #------------------------------------------------
-mins = []
+mins_loc = []
+cors_loc = []
 #------------------------------------------------
 nrot = 5
 #------------------------------------------------
 rb1 = [7,6,14,19]
 rb2 = [6,7,12,18]
 #------------------------------------------------
-for i in range(0, nrot):
+diangle = numpy.linspace(0.0, 2*math.pi, nrot, endpoint=False)
+nblock = int(math.ceil(float(nrot)/nproc) + 1.0e-10)
+diangle_loc = diangle[iproc*nblock:min((iproc+1)*nblock, nrot)]
+diangle_loc = diangle[iproc::nproc]
 #------------------------------------------------
-    for j in range(0, nrot):
+for angle_i in diangle_loc:
+#------------------------------------------------
+    for angle_j in diangle:
     #--------------------------------------------
-        print i, j
+        print angle_i, angle_j
         #--------------------------------------------
-        molrot = pybmol.clone
-        molrot.OBMol.SetTorsion(rb1[0],rb1[1],rb1[2],rb1[3], (2*math.pi/nrot)*i)
-        molrot.OBMol.SetTorsion(rb2[0],rb2[1],rb2[2],rb2[3], (2*math.pi/nrot)*j)
-        molrot = geomopt(molrot, 'mmff94s')
+        molr = pybmol.clone
+        molr.OBMol.SetTorsion(rb1[0],rb1[1],rb1[2],rb1[3], angle_i)
+        molr.OBMol.SetTorsion(rb2[0],rb2[1],rb2[2],rb2[3], angle_j)
+        molr = geomopt(molr, 'mmff94s')
         #--------------------------------------------
         unique = True
         #--------------------------------------------
-        for exmol in mins:
-            if (getRMSD(exmol, molrot) < 0.05):
+        for exmol in mins_loc:
+            if (getRMSD(exmol, molr) < 0.05):
                 unique = False
         if (unique == True):    
-            mins.append(molrot)
+            mins_loc.append(molr)
+            cors_loc.append(getCoords(molr))
         #--------------------------------------------
 
-#--------------------------------------------
-f = open("RMSD.dat", "w")
-#--------------------------------------------
-for i in range(0, len(mins)):
-    for j in range(0, len(mins)):
-        f.write("{:7.2f}".format(getRMSD(mins[i], mins[j])))
-    f.write("\n")
-#--------------------------------------------
-f.close()
-#--------------------------------------------
+#------------------------------------------------
+cors = MPI.COMM_WORLD.allgather(cors_loc)
+#------------------------------------------------
+mins = []
+#------------------------------------------------
+for i in range(0, nproc):
+#------------------------------------------------
+    for j in range(0, len(cors[i])):
+    #--------------------------------------------
+        molr = getPybmol(pybmol, cors[i][j])
+        #----------------------------------------
+        unique = True
+        #----------------------------------------
+        for exmol in mins:
+            if (getRMSD(exmol, molr) < 0.05):
+                unique = False
+        if (unique == True):    
+            mins.append(molr)
+        #----------------------------------------
+
+#------------------------------------------------
+if (iproc == 0):
+#------------------------------------------------
+    f = open("PRMSD.dat", "w")
+    #--------------------------------------------
+    for i in range(0, len(mins)):
+        for j in range(0, len(mins)):
+            f.write("{:7.2f}".format(getRMSD(mins[i], mins[j])))
+        f.write("\n")
+    #--------------------------------------------
+    f.close()
+    #--------------------------------------------
 
 
 #------------------------------------------------
 #          convert pymol to asemol              #
 #------------------------------------------------
-asemol = pyb2ase(pybmol)
+# asemol = pyb2ase(pybmol, iproc)
 #------------------------------------------------
 
 #-----------------------------------------
