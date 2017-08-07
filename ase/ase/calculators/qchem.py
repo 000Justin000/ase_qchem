75573a7c65c5cff4a161d54fdf3e3fdee099bddd
Junteng Jia <juntengjia@hotmail.com>
2017-08-06 21:52:45 -0400

    MP2 single point calculation

--- ase/ase/calculators/qchem.py
+++ ase/ase/calculators/qchem.py
@@ -11,12 +11,12 @@ import numpy as np
 from warnings import warn
 from ase.atoms import Atoms
 from ase.units import Hartree, Bohr
-from ase.io.qchem import write_qchem, read_qchem_opt_output
+from ase.io.qchem import write_qchem, read_qchem_opt_output, read_qchem_sp_output
 from ase.calculators.calculator import FileIOCalculator, Parameters, ReadError
 
 
 class QChem(FileIOCalculator):
-    implemented_properties = ['optimization']
+    implemented_properties = ['optimization, single_point']
     #-----------------------------------------
     command = 'qchem PREFIX.in PREFIX.out PREFIX.save > PREFIX.print'
     #-----------------------------------------
@@ -28,15 +28,17 @@ class QChem(FileIOCalculator):
     jobtype     = {'optimization' : 'OPT',
                    'frequency'    : 'FREQ',
                    'single_point' : 'SP'}
-    method      = {'B3LYP'  : 'B3LYP',
-                   'MP2'    : 'MP2'}
-    basis       = {'STO-3G' : 'STO-3G',
-                   '3-21G'  : '3-21G',
-                   '6-31G'  : '6-31G',
-                   '6-31G*' : '6-31G*'}
-    dft_d       = {'None'   : 'FALSE',
-                   'd2'     : 'EMPIRICAL_GRIMME',
-                   'd3'     : 'EMPIRICAL_GRIMME3'}
+    method      = {'B3LYP'        : 'B3LYP',
+                   'RIMP2'        : 'RIMP2'}
+    basis       = {'STO-3G'       : 'STO-3G',
+                   '3-21G'        : '3-21G',
+                   '6-31G'        : '6-31G',
+                   '6-31G*'       : '6-31G*',
+                   'aug-cc-pVDZ'  : 'aug-cc-pVDZ',
+                   'aug-cc-pVTZ'  : 'aug-cc-pVTZ'}
+    dft_d       = {'None'         : 'FALSE',
+                   'd2'           : 'EMPIRICAL_GRIMME',
+                   'd3'           : 'EMPIRICAL_GRIMME3'}
     #-----------------------------------------
 
     default_parameters = dict(
@@ -81,35 +83,42 @@ class QChem(FileIOCalculator):
         f = open(self.label + '.in', 'w')
         write_qchem(f, atoms, p.comment)
         
-        if p.tcs is not None:
-            f.write("$opt\n")
-            f.write("CONSTRAINT\n")
-            for tc in p.tcs:
-                di_angle = tc[1]*360/(2*math.pi)
-                di_angle = np.mod(di_angle+180.0, 360.0)-180.0
-                f.write("tors    " + str(tc[0][0]) + "    " + str(tc[0][1]) + "    " + str(tc[0][2]) + "    " + str(tc[0][3]) + "    " + str(di_angle) + "\n")
-            f.write("ENDCONSTRAINT\n")
-            f.write("$end\n\n")
+        if (p.task == "optimization"):
+            if p.tcs is not None:
+                f.write("$opt\n")
+                f.write("CONSTRAINT\n")
+                for tc in p.tcs:
+                    di_angle = tc[1]*360/(2*math.pi)
+                    di_angle = np.mod(di_angle+180.0, 360.0)-180.0
+                    f.write("tors    " + str(tc[0][0]) + "    " + str(tc[0][1]) + "    " + str(tc[0][2]) + "    " + str(tc[0][3]) + "    " + str(di_angle) + "\n")
+                f.write("ENDCONSTRAINT\n")
+                f.write("$end\n\n")
 
         f.write("$rem\n")
-        f.write("JOBTYPE             " + self.jobtype[p.task]    + "\n")
-        f.write("METHOD              " + self.method[p.xc]       + "\n")
-        f.write("DFT_D               " + self.dft_d[p.disp]      + "\n")
-        f.write("BASIS               " + self.basis[p.basis]     + "\n")
+        f.write("JOBTYPE             "     + self.jobtype[p.task]    + "\n")
+        f.write("METHOD              "     + self.method[p.xc]       + "\n")
+        f.write("DFT_D               "     + self.dft_d[p.disp]      + "\n")
+        f.write("BASIS               "     + self.basis[p.basis]     + "\n")
         if (p.task == "optimization"):
-            f.write("GEOM_OPT_MAX_CYCLES " + str(p.opt_maxcycle) + "\n")
-        f.write("SYMMETRY            " + str(p.symmetry)         + "\n")
-        f.write("THRESH              " + str(p.thresh)           + "\n")
-        f.write("SCF_CONVERGENCE     " + str(p.scf_convergence)  + "\n")
-        f.write("MAX_SUB_FILE_NUM    " + str(p.maxfile)          + "\n")
-        f.write("MEM_STATIC          " + str(p.mem_static)       + "\n")
-        f.write("MEM_TOTAL           " + str(p.mem_total)        + "\n")
+            f.write("GEOM_OPT_MAX_CYCLES " + str(p.opt_maxcycle)     + "\n")
+        if (p.xc == "RIMP2"):
+            f.write("AUX_BASIS           " + "RIMP2-" + self.basis[p.basis] + "\n")
+        if (p.xc in ["RIMP2"]):
+            f.write("N_FROZEN_CORE       " + "FC"                    + "\n")
+        f.write("SYMMETRY            "     + str(p.symmetry)         + "\n")
+        f.write("THRESH              "     + str(p.thresh)           + "\n")
+        f.write("SCF_CONVERGENCE     "     + str(p.scf_convergence)  + "\n")
+        f.write("MAX_SUB_FILE_NUM    "     + str(p.maxfile)          + "\n")
+        f.write("MEM_STATIC          "     + str(p.mem_static)       + "\n")
+        f.write("MEM_TOTAL           "     + str(p.mem_total)        + "\n")
         f.write("$end\n")
 
     def read_output(self):
         p = self.parameters
         if (p.task == "optimization"):
             return read_qchem_opt_output(self.label + ".out")
+        elif (p.task == "single_point"):
+            return read_qchem_sp_output(self.label + ".out")
 
     def run(self, atoms):
         self.write_input(atoms)
